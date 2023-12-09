59ce7cf19b50e2430ffdb0eab137105cba948ad6
Tracy-miranja <barazatracy44@gmail.com>
2023-12-10 00:58:00 +0300

    Add banner section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import {Button, Badge }from "react-bootstrap";
 // import clean from '../../assets/clean.png'
 import '../Home/home.css';
 
@@ -8,16 +9,24 @@ const Home = () => {
       <div className="slanted-container">
         <div className="row">
           <div className="col-md-4 section-a">
-            <h4>DROP & PICK AFTER 24HRS</h4>
-            <h2> PROFESSIONAL <br />
-CARPET CLEANING</h2>
+            <div className="content">
+            <h6>DROP & PICK AFTER 24HRS</h6>
+            <h1> PROFESSIONAL <br />
+                CARPET CLEANING
+                </h1>
             <h6>
             Lorem Ipsum is simply dummy text of the printing 
 and typesetting industry. Lorem Ipsum has been the
- industry's standard dummy text ever since the 1500s, 
+ industry standard dummy text ever since the 1500s, 
 when an unknown printer took a galley of type and
  scrambled it to make a type specimen book.
             </h6>
+            <Button variant="secondary">
+              Learn More<Badge bg="secondary"></Badge>
+              <span className="visually-hidden">unread messages</span>
+            </Button>
+            </div>
+           
           </div>
           <div className="col-md-8 section-b">
             <div className="inner"></div>
