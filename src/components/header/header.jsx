f3d59c8aa8de2b838c0d5c43450af83515c8d6cc
Tracy-miranja <barazatracy44@gmail.com>
2023-12-12 11:49:08 +0300

    Add footer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,27 +11,27 @@ import Button from 'react-bootstrap/Button';
 const Header = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary navbar-sticky">
         <Container fluid className="navsection">
           <div>
             <Navbar.Brand href="Home">
               <img src={logo} alt="Essica" />
             </Navbar.Brand>
-            </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          
-          <div>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/About">About</Nav.Link>
-              <Nav.Link href="/Services">Service</Nav.Link>
-              <Nav.Link href="/Blog">Blog</Nav.Link>
-              <Nav.Link href="/Contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
           </div>
-          <div className="d-lg"> 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <div>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/About">About</Nav.Link>
+                <Nav.Link href="/Services">Service</Nav.Link>
+                <Nav.Link href="/Blog">Blog</Nav.Link>
+                <Nav.Link href="/Contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+          <div className="d-lg">
             <Button variant="primary">
               Book Now<Badge bg="secondary"></Badge>
               <span className="visually-hidden">unread messages</span>
