5e10488f1e28a5a7c78f0add2cac2eae775a86f4
Tracy-miranja <barazatracy44@gmail.com>
2023-12-13 12:23:07 +0300

    Add blog page

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import { Nav } from "react-bootstrap"
+import '../Blog/blog.css'
 
 const Blog=() =>{
   return (
@@ -10,6 +11,40 @@ const Blog=() =>{
             <h1 >BLOG</h1>
             <Nav.Link href="/">Home </Nav.Link>
           </div>
+        </div>
+      </div>
+      <div className="blog-section">
+        <p>Blog & Article</p>
+        <h2>Read Our Latest <br />
+    Blog & Article</h2>
+      </div>
+      <div className="blog-post">
+        <div className="blog-article">
+          <div className="blog-image"></div>
+          <div>
+            <h4>How To Create An Organized Cleaning Schedule That Works</h4><p>Lorem ipsum dolor sit amet,
+ consectetur adipiscing elit. 
+Ut elit tellus, luctus nec 
+ullamcorper mattis, </p></div>
+        </div>
+        <div className="blog-article">
+        <div className="blog-image"></div>
+          <div>
+            <h4>Green Cleaning, Eco-Friendly Solutions For A Healthier Home</h4>
+            <p>Lorem ipsum dolor sit amet,
+ consectetur adipiscing elit. 
+Ut elit tellus, luctus nec 
+ullamcorper mattis, </p>
+</div>
+        </div>
+        <div className="blog-article">
+        <div className="blog-image"></div>
+          <div>
+            <h4>Removing Stubborn Stains, A Comprehensive Guide</h4>
+            <p>Lorem ipsum dolor sit amet,
+ consectetur adipiscing elit. 
+Ut elit tellus, luctus nec 
+ullamcorper mattis, </p></div>
         </div>
       </div>
     </div>
