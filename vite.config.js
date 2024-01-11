8f285834f43d528966432ca001fc53262588c78f
Tracy-miranja <barazatracy44@gmail.com>
2024-01-11 18:00:20 +0300

    Add netlify file

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-  export default defineConfig({
+export default defineConfig({
+  // base:"/essica-carpet-cleaning/",
   plugins: [react()],
-  build: {
-    assetsInclude: ['_redirects'],
-  },
 })
