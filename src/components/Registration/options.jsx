f79d00f606a475840d08ca5445bddebccd57b8aa
Tracy-miranja <barazatracy44@gmail.com>
2024-02-17 12:46:44 +0300

    Add forget password

--- src/components/Registration/options.jsx
+++ src/components/Registration/options.jsx
@@ -1,7 +1,7 @@
 // import  from 'react';
-import SignIn from '../Signin/Signin';
-import './options.css';
-import { useNavigate } from 'react-router-dom';
+import SignIn from "../Signin/Signin";
+import "./options.css";
+import { useNavigate } from "react-router-dom";
 
 const Options = () => {
   const navigate = useNavigate();
@@ -10,24 +10,30 @@ const Options = () => {
     navigate(path);
   };
 
-return (
+  return (
     <>
-        <div className="options-container">
-            <div className="button-group">
-            <h1>Welcome 💃</h1>
-              <SignIn/>
-                {/* <p>If you have already registered, please click below to make a booking</p>
+      <div className="options-container">
+        <div className="button-group">
+          <h1>Welcome 💃</h1>
+          <SignIn />
+          <a href="#">Forgot password?</a>
+          {/* <p>If you have already registered, please click below to make a booking</p>
                 <button className="options-button" onClick={() => navigateTo('/signin')}>Sign In to Book</button> */}
-                <p>If you haven&apos;t registered, please, click below to register</p>
-               
-                <a href="/signup" className="options-link" role="button" aria-pressed="false" onClick={() => navigateTo('/signup')}>
-  Sign Up to Book
-</a>
+          <p>If you haven&apos;t registered, please, click below to register</p>
 
-            </div>
+          <a
+            href="/signup"
+            className="options-link"
+            role="button"
+            aria-pressed="false"
+            onClick={() => navigateTo("/signup")}
+          >
+            Sign Up to Book
+          </a>
         </div>
+      </div>
     </>
-);
+  );
 };
 
 export default Options;
